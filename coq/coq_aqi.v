e24a4fd180fc7c676e0fd61ededbdc6dd2c4894d
dhwcmoore <dhwcmoore@gmail.com>
2025-06-18 04:36:26 -0600

    Checkpoint before continuing OCaml setup

--- coq/coq_aqi.v
+++ coq/coq_aqi.v
@@ -1,12 +1,9 @@
-(* coq/coq_aqi.v *)
-Require Import ZArith.
+(** AQI classification logic *)
 
-Open Scope Z_scope.
-
-Definition AQI_Category (x : Z) : string :=
-  if x <=? 50 then "Good"
-  else if x <=? 100 then "Moderate"
-  else if x <=? 150 then "Unhealthy for Sensitive Groups"
-  else if x <=? 200 then "Unhealthy"
-  else if x <=? 300 then "Very Unhealthy"
-  else "Hazardous".
+Definition classify_aqi (aqi : Z) : string :=
+  if (aqi <? 50)%Z then "Good" else
+  if (aqi <? 100)%Z then "Moderate" else
+  if (aqi <? 150)%Z then "Unhealthy for Sensitive Groups" else
+  if (aqi <? 200)%Z then "Unhealthy" else
+  if (aqi <? 300)%Z then "Very Unhealthy" else
+  "Hazardous".
